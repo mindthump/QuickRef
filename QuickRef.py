97e136f0924a065d567f1b95579df5ba6d46cc9c
Ed Cardinal <edcgit@mindthump.com>
2015-05-09 13:07:48 -0700

    Added deal() to show BitVector module.

--- QuickRef.py
+++ QuickRef.py
@@ -46,6 +46,12 @@ BDAC
 DBAC
 ABDC
 BADC
+>>> # Be more specific with ellipsis on shuffled deck?
+>>> q.deal() # doctest: +ELLIPSIS
+Unshuffled:
+['AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD']
+Shuffled:
+...
 >>> main('Ed')
 Welcome to Ed's World!!!
 ['apples', 'hamburgers', 'grasshoppers']
@@ -56,6 +62,9 @@ Result 108 is over one hundred
 """
 
 import sys, os  # load common libraries
+from BitVector import BitVector
+import random
+import copy
 
 
 def main(parameter):
@@ -100,6 +109,16 @@ asdfasdf
 klasdflhf
 asfd
 sdfdsf""")
+        # BitVector stuff
+        self.deck = []
+        self.stack = ['8', 'K', '3', '10', '2', '7', '9', '5', 'Q', '4', 'A', '6', 'J']
+        for suit in "CHSD":
+            self.deck.append("A" + suit)
+            for pip in range(2,11):
+                self.deck.append(str(pip) + suit)
+            for pip in "JQK":
+                self.deck.append(str(pip) + suit)
+
 
     def param(self):
         """Simple printing of the parameters."""
@@ -193,6 +212,18 @@ sdfdsf""")
                 j = i if (n % 2 == 0) else 0
                 # Cool python swap trick
                 array[j], array[n - 1] = array[n - 1], array[j]
+
+    def deal(self):
+        print("Unshuffled:")
+        print(self.deck)
+        shuffled = copy.copy(self.deck)
+        random.shuffle(shuffled)
+        print("Shuffled:")
+        print(shuffled)
+        dbv = BitVector(size = 52)
+        for y in shuffled:
+            dbv[self.deck.index(y)] = 1
+            print(dbv)
 
 
 if __name__ == "__main__":
--- whiteboard.py
+++ whiteboard.py
@@ -1,13 +1,44 @@
+""" This 'whiteboard' file is for experimentation and concept validation only; it can change at any time.
+
+>>> deck = Solution()
+>>> deck.deal() # doctest: +ELLIPSIS
+Unshuffled:
+...
+Shuffled:
+...
+
 """
->>> s = Solution()
->>> s.makeDict(['spam',10,15,'zzz','eggz'])
-<type 'dict'>
-{0: 'spam', 1: 10, 2: 15, 3: 'zzz', 4: 'eggz'}
-"""
+
+from BitVector import BitVector
+import random
+import copy
+
 
 class Solution:
 
-    def makeDict(self, alist):
-        d = dict(enumerate(alist))
-        print type(d)
-        print d
+    def __init__(self):
+        self.deck = []
+        self.stack = ['8', 'K', '3', '10', '2', '7', '9', '5', 'Q', '4', 'A', '6', 'J']
+        for suit in "CHSD":
+            self.deck.append("A" + suit)
+            for pip in range(2,11):
+                self.deck.append(str(pip) + suit)
+            for pip in "JQK":
+                self.deck.append(str(pip) + suit)
+
+    def deal(self):
+        print("Unshuffled:")
+        print(self.deck)
+        shuffled = copy.copy(self.deck)
+        random.shuffle(shuffled)
+        print("Shuffled:")
+        print(shuffled)
+        dbv = BitVector(size = 52)
+        for y in shuffled:
+            dbv[self.deck.index(y)] = 1
+            print(dbv)
+
+
+if __name__ == '__main__':
+    deck = Solution()
+    deck.deal()
